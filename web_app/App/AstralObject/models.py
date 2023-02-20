f10a9c4d6ed2570d9cf5bbb2d3c9764549a2b2dd
Hazeek <101054526+HazeekAhamed@users.noreply.github.com>
2023-02-20 22:09:33 +0530

    migrated the changes

--- web_app/App/AstralObject/models.py
+++ web_app/App/AstralObject/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-#from django.contrib.auth.models import User
+
+
+# from django.contrib.auth.models import User
 
 # Create your models here.
-class Astral(models.Model): 
-    #All are text inputs   
-    astral_id = models.IntegerField(primary_key=True,default=None) 
-    created_by = models.CharField(max_length=200,default=None)
+class Astral(models.Model):
+    # All are text inputs
+    astral_id = models.AutoField(primary_key=True)
+    created_by = models.CharField(max_length=200, default=None)
     ultraviolet_filter = models.CharField(max_length=30)
     green_filter = models.CharField(max_length=30)
     near_infrared_filter = models.CharField(max_length=30)
@@ -14,5 +16,4 @@ class Astral(models.Model):
     modified_julian_date = models.CharField(max_length=30)
     spectroscopic_objects_id = models.CharField(max_length=30)
     redshift_value = models.CharField(max_length=30)
-    plate_id = models.CharField(max_length=30) 
-
+    plate_id = models.CharField(max_length=30)
