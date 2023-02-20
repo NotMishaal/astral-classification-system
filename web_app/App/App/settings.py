3cbb462c510ba55f238e71e72e713af7884bfea3
Hazeek <101054526+HazeekAhamed@users.noreply.github.com>
2023-02-20 17:22:16 +0530

    changed the database from sqlite3 to MySQL

--- web_app/App/App/settings.py
+++ web_app/App/App/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'App.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'astral_classification_system',
+        'USER': 'root',  # use the MySQL root user
+        'PASSWORD': '',  # no password by default
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
